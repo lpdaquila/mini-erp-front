7014c2ee8d88f338118f178e0c23ca4433cfc5d0
lpdaquila <99849531+lpdaquila@users.noreply.github.com>
2025-06-24 18:38:01 -0300

    initial: creating the frontend of mini-erp project

---
+++ src/utils/api.ts
@@ -0,0 +1,33 @@
+import axios, { AxiosError } from "axios";
+import { ApiError } from "src/models/Api";
+
+
+export async function useApi<TypeDataResponse>(
+    endpoint: string,
+    method: 'GET' | 'POST' | 'PUT' | 'DELETE' = 'GET',
+    data?: object,
+    withAuth: boolean = true
+): Promise<{
+    data?: TypeDataResponse,
+    detail: string
+}> {
+    try {
+        const request = await axios(`${process.env.API_URL}/${endpoint}`, {
+            method,
+            data: method != 'GET' && data,
+            params: method == 'GET' && data
+        })
+
+        return {
+            data: request.data,
+            detail: ''
+        }
+    } catch (e) {
+        const error = e as AxiosError<ApiError>;
+
+        return {
+            data: null,
+            detail: error.response.data.detail || error.message
+        }
+    }
+}
