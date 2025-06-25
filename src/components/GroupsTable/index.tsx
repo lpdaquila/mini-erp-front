ab00f5234f27102b2f13f5e7b79f97f8a33f9d9a
lpdaquila <99849531+lpdaquila@users.noreply.github.com>
2025-06-25 16:11:56 -0300

    feat: finished the components 'Groups' and 'Permissions'

---
+++ src/components/GroupsTable/index.tsx
@@ -0,0 +1,99 @@
+import {
+    Card,
+    Container,
+    IconButton,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Tooltip,
+    Typography
+} from "@mui/material";
+import EditTwoToneIcon from '@mui/icons-material/EditTwoTone'
+import { DeleteTwoTone } from "@mui/icons-material";
+import { useTheme } from "@mui/styles";
+import { useNavigate } from "react-router";
+import { GroupDetail } from "src/models/Group";
+import { useAuth } from "src/utils/auth";
+import { useRequests } from "src/utils/requests";
+
+type Props = {
+    groupsList: GroupDetail[];
+    refreshList: () => void;
+}
+
+export default function GroupsTable({ groupsList, refreshList }: Props) {
+    const { handlePermissionsExists } = useAuth();
+
+    const { deleteGroup } = useRequests();
+
+    const theme = useTheme();
+    const navigate = useNavigate();
+
+    function handleEditGroup(id: number) {
+        navigate(`/groups/edit/${id}`)
+    }
+
+    async function handleDeleteGroup(id: number) {
+        await deleteGroup(id);
+        refreshList();
+    }
+
+    return (
+        <Container maxWidth='lg'>
+            <Card>
+                <TableContainer>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>ID</TableCell>
+                                <TableCell>Name</TableCell>
+                                <TableCell align="right">Actions</TableCell>
+                            </TableRow>
+                            <TableBody>
+                                {groupsList.map((group) => (
+                                    <TableRow hover key={group.id}>
+                                        <TableCell>
+                                            <Typography
+                                                fontWeight="bold"
+                                                gutterBottom
+                                            >
+                                                #{group.id}
+                                            </Typography>
+                                        </TableCell>
+                                        <TableCell>
+                                            <Typography
+                                                fontWeight="bold"
+                                                gutterBottom
+                                            >
+                                                #{group.name}
+                                            </Typography>
+                                        </TableCell>
+                                        <TableCell align="right">
+                                            {handlePermissionsExists('change_group') &&
+                                                <Tooltip title="Edit role" arrow>
+                                                    <IconButton>
+                                                        <EditTwoToneIcon onClick={() => handleEditGroup(group.id)} />
+                                                    </IconButton>
+                                                </Tooltip>
+                                            }
+                                            {handlePermissionsExists('delete_group') &&
+                                                <Tooltip title="Delete role" arrow>
+                                                    <IconButton>
+                                                        <DeleteTwoTone onClick={() => handleDeleteGroup(group.id)} />
+                                                    </IconButton>
+                                                </Tooltip>
+                                            }
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </TableHead>
+                    </Table>
+                </TableContainer>
+            </Card>
+        </Container>
+    )
+}
---
+++ src/components/PermissionsList/index.tsx
@@ -0,0 +1,40 @@
+import { Checkbox, FormControlLabel, FormGroup } from "@mui/material";
+import { ChangeEvent } from "react";
+import { PermissionDetail } from "src/models/Permission";
+
+type Props = {
+    permissionsData: PermissionDetail[];
+    selectedPermissions: number[];
+    setSelectedPermissions: (value: number[]) => void;
+}
+
+export default function PermissionsList({
+    permissionsData, selectedPermissions, setSelectedPermissions }: Props) {
+
+    function handleChange(event: ChangeEvent<HTMLInputElement>, permission_id: number) {
+        const { checked } = event.target;
+
+        if (checked) {
+            setSelectedPermissions([...selectedPermissions, permission_id])
+        } else {
+            setSelectedPermissions(selectedPermissions.filter(fId => fId != permission_id))
+        }
+    }
+
+    return (
+        <FormGroup>
+            {permissionsData.map((item) => (
+                <FormControlLabel
+                    key={item.id}
+                    control={
+                        <Checkbox
+                            checked={selectedPermissions.find((id) => id === item.id) != undefined}
+                            onChange={(e) => handleChange(e, item.id)}
+                        />
+                    }
+                    label={item.name}
+                />
+            ))}
+        </FormGroup>
+    )
+} 
